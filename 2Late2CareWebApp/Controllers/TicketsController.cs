8a8399add0420d4340c1fad06a9d9ef6afa350f2
Maxime MONNIER <maxime.monnier@epsi.fr>
2020-01-06 17:00:36 +0100

    separation front back

---
+++ 2Late2CareWebApp/Controllers/TicketsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace _2Late2CareWebApp.Controllers
+{
+    public class TicketsController : ApiController
+    {
+        // GET api/tickets
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ticket1\n", "ticket2\n", "ticket3\n" };
+        }
+
+        // GET api/tickets/5
+        public string Get(int id)
+        {
+            StringBuilder sbr = new StringBuilder();
+            DateTime localDate = DateTime.Now;
+            var culture = new CultureInfo("fr-FR");
+            sbr.Append("ticket : ");
+            sbr.Append(id);
+            sbr.AppendLine();
+            sbr.Append(culture.NativeName);
+            sbr.AppendLine();
+            sbr.Append(localDate.ToString(culture));
+            return sbr.ToString();
+        }
+
+        // POST api/tickets
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/tickets/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/tickets/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
